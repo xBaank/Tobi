e19b479996999707156088fea0b29c5742637476
Roberto Blázquez <elroby444@gmail.com>
2023-05-03 13:41:13 +0200

    Add cookiesettings to test

--- DiscordBot.MusicPlayer.Tests/Services/YoutubeServiceTest.cs
+++ DiscordBot.MusicPlayer.Tests/Services/YoutubeServiceTest.cs
@@ -6,12 +6,17 @@ using FluentAssertions;
 using MusicPlayer.Services;
 using Xunit;
 using YoutubeExplode;
-
+using static System.Environment;
 public class YoutubeServiceTest
 {
     private readonly IMusicService _musicService;
 
-    public YoutubeServiceTest() => _musicService = new YoutubeService(new YoutubeClient());
+    public YoutubeServiceTest() {
+        var sapisid = GetEnvironmentVariable("Sapisid") ?? throw new Exception("Sapisid not found")
+        var psid = GetEnvironmentVariable("Psid") ?? throw new Exception("Psid not found")
+        var cookiesettings = new CookiesSettings(sapisid, psid)
+        _musicService = new YoutubeService(new YoutubeClient(cookiesettings));
+    }
 
     [Fact]
     public async Task GetSongsByQueryReturnsSong()
